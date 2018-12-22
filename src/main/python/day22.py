3623d11d437539f4b2e4a23592e4066beee623a0
Bouke Versteegh <info@boukeversteegh.nl>
2018-12-22 21:21:53 +0100

    day 22b wip

--- src/main/python/day22.py
+++ src/main/python/day22.py
@@ -1,9 +1,28 @@
+import collections
+import sys
+from collections import defaultdict
+from timeit import default_timer as timer
+import heapq
+
+t0 = timer()
+
+# input
 target = 12, 757  # x,y
 mouth = (0, 0)
 depth = 3198
+
+# example
+target = 10, 10
+depth = 510
+
 GI = {}
 EL = {}
 types = ['.', '=', '|']  # rocky wet narrow
+rocky = 0
+wet = 1
+narrow = 2
+
+tx, ty = target
 
 
 def geologic_index(c):
@@ -44,24 +63,93 @@ def region_type(c):
     return erosion_level(c) % 3
 
 
-import collections
-import sys
+grid = collections.defaultdict(lambda: {})  # type: defaultdict[int, Dict[int, int]]
 
-grid = collections.defaultdict(lambda: {})
 
-risklevel = 0
+def g(x, y):
+    try:
+        return grid[y][x]
+    except KeyError:
+        t = region_type((x, y))
+        grid[y][x] = t
+        return t
+
+
 for y in xrange(mouth[1], target[1] + 1):
     for x in xrange(mouth[0], target[0] + 1):
         t = region_type((x, y))
         grid[y][x] = t
-        risklevel += t
+
+torch = 0
+climbing = 1
+neither = 2
+
+type_tools = {
+    rocky: [climbing, torch],
+    wet: [climbing, neither],
+    narrow: [torch, neither]
+}
+
+
+class Node:
+    def __init__(self, x, y, t, c):
+        self.t = t
+        self.y = y
+        self.x = x
+        self.c = c
+        self.h = abs(x - tx) + abs(y - ty) - 1
+        self.f = c + self.h
+
+    def is_valid(self):
+        if self.x < 0 or self.y < 0:
+            return False
+        return self.t in type_tools[g(self.x, self.y)]
+
+    def is_goal(self):
+        return self.x == tx and self.y == ty and self.t == torch
+
+    def tuple(self):
+        return self.x, self.y, self.t, self.c
+
+
+cnode = None
+nodes = [
+    Node(mouth[0], mouth[1], torch, 0)
+]
+
+visited_nodes = set()
+
+
+def expand(n):
+    global nodes
+    x, y, t, c = n.tuple()
+
+    children = [
+        Node(x + 1, y, t, c + 1),
+        Node(x - 1, y, t, c + 1),
+        Node(x, y + 1, t, c + 1),
+        Node(x, y - 1, t, c + 1),
+        Node(x, y, t, c + 1),
+        Node(x, y, (t + 1) % 3, c + 7),
+        Node(x, y, (t + 2) % 3, c + 7),
+    ]
+    new_children = [cn for cn in children if (cn.x, cn.y, cn.t) not in visited_nodes]
+
+    for child in new_children:
+        nodes.insert(0, child)
+    # print len(nodes),'-',
+    #
+    # print '-', len(nodes)
+
 
 def show():
     for y in xrange(mouth[1], target[1] + 1):
         for x in xrange(mouth[0], target[0] + 1):
-            t = grid[y][x]
+            t = g(x, y)
             if (x, y) == mouth:
                 sys.stdout.write('M')
+            if cnode and (x, y) == (cnode.x, cnode.y):
+                sys.stdout.write(['*', '^', '_'][cnode.t])
             elif (x, y) == target:
                 sys.stdout.write('T')
             else:
@@ -69,4 +157,32 @@ def show():
         print
 
 
-print risklevel
+show()
+
+while len(nodes):
+    T = min(len(nodes), 100)
+    # nodes = sorted(nodes[:T], key=lambda _n: _n.f) + nodes[T:]
+
+    nodes.sort(key=lambda _n: _n.f)
+    n = nodes[0]
+    cnode = n
+
+    visited_nodes.add((n.x, n.y, n.t))
+
+    if n.is_goal():
+        print
+        show()
+        print {"goal": n.tuple()}
+        break
+
+    if n.is_valid():
+        end = timer()
+        rtime = end - t0
+        print "\r#%s" % len(visited_nodes), (n.x, n.y, n.c, n.h), ('t=%d' % rtime), ('c/t=%02f' % (n.c / rtime)),
+        expand(n)
+
+    nodes.remove(n)
+
+    # break
+    # for node in nodes:
+    #     print node.tuple()
